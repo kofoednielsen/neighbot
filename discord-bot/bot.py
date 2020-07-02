7693bd0979ed30091abc52ada831b857ec76099a
AsbjornOlling <asbjornolling@gmail.com>
2020-07-02 22:29:54 +0200

    done

---
+++ discord-bot/bot.py
@@ -0,0 +1,27 @@
+
+import discord  # type: ignore
+import rabbitmq  # type: ignore
+import pika  # type: ignore
+import loguru  # type: ignore
+from os import environ
+
+# setup rabbitmq shit
+connection = pika.BlockingConnection(pika.ConnectionParameters("amqp"))
+channel = connection.channel()
+channel.queue_declare(queue="discordbot")
+channel.basic_consume(queue="discordbot",on_message_callback=send_to_discord, auto_ack=True)
+
+# discord shit
+bot = discord.Client()
+
+
+@bot.event
+async def on_ready():
+    logger.info(f'{client.user} has connected to Discord!')
+
+
+def send_to_discord(ch, method, properties, body):
+    # forward message to discord channel
+    logger.info(f"Sending to discord: {body}")
+    channel = bot.get_channel(environ["CHANNEL_ID"])
+    channel.send(body)
