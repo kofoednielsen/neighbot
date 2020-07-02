c464ebc1480731ffb64827785624be1967b737f4
AsbjornOlling <asbjornolling@gmail.com>
2020-07-02 22:41:56 +0200

    booop

--- discord-bot/bot.py
+++ discord-bot/bot.py
@@ -1,33 +1,29 @@
 import discord  # type: ignore
 import pika  # type: ignore
-import loguru  # type: ignore
+from loguru import logger # type: ignore
 from os import environ
+from threading import Thread
 
 # setup rabbitmq shit
 connection = pika.BlockingConnection(pika.ConnectionParameters("amqp"))
 channel = connection.channel()
 channel.queue_declare(queue="discordbot")
-
-# discord shit
-bot = discord.Client()
-
-
-@bot.event
-async def on_ready():
-    logger.info(f"{client.user} has connected to Discord!")
-
+                                       #  ░█▀▄░█▀█░█▀█░█▀█
+# discord shit                         #  ░█▀▄░█░█░█░█░█▀▀
+bot = discord.Client()                 #  ░▀▀░░▀▀▀░▀▀▀░▀░░
 
 def send_to_discord(ch, method, properties, body):
     # forward message to discord channel
-    logger.info(f"Sending to discord: {body}")
-    channel = bot.get_channel(environ["CHANNEL_ID"])
-    channel.send(body)
+    print(f"Sending to discord: {body}")
+    bot.get_channel(environ["CHANNEL_ID"]).send(body)
 
+def start_consumer():
+    channel.basic_consume(queue="discordbot", on_message_callback=send_to_discord, auto_ack=True)
+    channel.start_consuming()
+
+def start_discord_bot():
+    bot.run(environ["DISCORD_TOKEN"])
 
 if __name__ == "__main__":
-    channel.basic_consume(
-        queue="discordbot",
-        on_message_callback=send_to_discord,
-        auto_ack=True
-    )
-    channel.start_consuming()
+    Thread(target=start_discord_bot).start()
+    start_consumer()
